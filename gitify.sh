60dd3bbe2913d6494116c388e5d95bf417cfad19
Ray C Horn <raychorn@gmail.com>
2020-10-15 12:29:48 -0600

    next  commit

--- gitify.sh
+++ gitify.sh
@@ -5,5 +5,5 @@ find * -size +4M -type f -print >> .gitignore
 git add -A
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://raychorn:02bf4fccf51cd91f3753d1101ce867cd9a6ae33c@github.com/raychorn/svn_openlaszlo-apps.git
+git remote add origin https://raychorn:3696beb6ac9b3783c840518182a6dfd2955f670e@github.com/raychorn/svn_openlaszlo-apps.git
 git push -u origin main
